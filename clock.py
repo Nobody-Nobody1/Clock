a0a98f1f8a51cb032dc7d8b90067c26ffd797107
Nobody_Nobody1 <bibhasn123@gmail.com>
2026-01-27 12:26:09 -0800

    Update clock.py

--- clock.py
+++ clock.py
@@ -1,11 +1,6 @@
 import tkinter as tk
 root = tk.Tk()
-root.title("Keep Window on Top")
-root.geometry("300x300")
-# Create a new window
-top_window = tk.Toplevel(root)
-top_window.title("Top Window")
-top_window.geometry("200x200")
-# Set the top_window on top of the root window
-top_window.wm_attributes("-topmost", True)
+root.title("Clock")
+root.geometry("300x100")
+root.wm_attributes("-topmost", True)
 root.mainloop()
